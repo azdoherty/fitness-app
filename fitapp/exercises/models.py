60c4e91005fda282e0ee0ab2dd3e9b3ad817e069
adoherty8 <azdoherty91@gmail.com>
2020-03-18 08:01:55 -0400

    add some many to many fields

--- fitapp/exercises/models.py
+++ fitapp/exercises/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+
 # Create your models here.
 
 
@@ -12,9 +14,21 @@ class Equipment(models.Model):
 class Exercise(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500)
-    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE, null=True)
+    instruction = models.CharField(max_length=255, default='')
+    instructions = ArrayField(instruction, default=list)
+    
+    def __str__(self):
+        return self.title
+
 
 class MovementStep(models.Model):
+    Exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     order = models.IntegerField()
     description = models.CharField(max_length=500)
     image = models.FileField()
+
+
+class Workout(models.Model):
+    name = models.CharField
+    exercises = models.ManyToManyField(Exercise)
--- fitapp/exercises/views.py
+++ fitapp/exercises/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from .models import Exercise, MovementStep
 # Create your views here.
+
+
+def 
